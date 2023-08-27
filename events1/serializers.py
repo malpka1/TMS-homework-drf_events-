a54991c7b4d02ac69e5d1de69c70a7a17af3f512
malpka1 <128837407+malpka1@users.noreply.github.com>
2023-08-27 21:57:08 +0200

    Add JWT
    добавила аутентификацию

--- events1/serializers.py
+++ events1/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers, permissions
 
 from django.contrib.auth.models import User
 
@@ -6,15 +6,17 @@ from events1.models import Event
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    permission_classes = [permissions.IsAuthenticated]
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'email', 'password')
 
     def create(self, validated_data):
-        user = User(
-            email=validated_data['email'],
-            username=validated_data['username']
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
         )
         user.set_password(validated_data['password'])
         user.save()
